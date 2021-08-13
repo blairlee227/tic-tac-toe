8035a07b2246ba960897c7d050e4554bc610de82
blairlee227 <blairlee227@gmail.com>
2021-08-13 11:21:18 +0800

    update. 判斷方法

--- tictactoe.js
+++ tictactoe.js
@@ -16,23 +16,19 @@ function select(itemId) {
     if (!gameOver) {
       // 判斷要在哪畫 O
       // 1. 判斷是否有即將連線的 O
-      // 先找出不是 undefined 的
-      let selectedX = [];
-      let selectedO = [];
-      for (let i = 1; i < canvas.length; i++) {
-        if (canvas[i] === "X") selectedX.push(i);
-        if (canvas[i] === "O") selectedO.push(i);
-      }
-      console.log("X", selectedX);
-      console.log("O", selectedO);
-      // 123 456 789
-      // 147 258 369
-      // 159 357
-
       // 2. 判斷是否有即將連線的 X
+      // 先找出不是 undefined 的
+      // let selectedX = [];
+      // let selectedO = [];
+      // for (let i = 1; i < canvas.length; i++) {
+      //   if (canvas[i] === "X") selectedX.push(i);
+      //   if (canvas[i] === "O") selectedO.push(i);
+      // }
+      // console.log("X", selectedX);
+      // console.log("O", selectedO);
+
       // 3. 在剩下的格子中隨機畫 O
       setO();
-      console.log("canvas", canvas);
       // 再次判斷是否連線
       check();
     }
@@ -41,48 +37,87 @@ function select(itemId) {
 
 // 判斷是否連線
 function check() {
-  if (canvas[1] === canvas[2] && canvas[2] === canvas[3]) {
-    setStyle(1, canvas[1]);
-    setStyle(2, canvas[2]);
-    setStyle(3, canvas[3]);
-  } else if (canvas[4] === canvas[5] && canvas[5] === canvas[6]) {
-    setStyle(4, canvas[4]);
-    setStyle(5, canvas[5]);
-    setStyle(6, canvas[6]);
-  } else if (canvas[7] === canvas[8] && canvas[8] === canvas[9]) {
-    setStyle(7, canvas[7]);
-    setStyle(8, canvas[8]);
-    setStyle(9, canvas[9]);
-  } else if (canvas[1] === canvas[4] && canvas[4] === canvas[7]) {
-    setStyle(1, canvas[1]);
-    setStyle(4, canvas[4]);
-    setStyle(7, canvas[7]);
-  } else if (canvas[2] === canvas[5] && canvas[5] === canvas[8]) {
-    setStyle(2, canvas[2]);
-    setStyle(5, canvas[5]);
-    setStyle(8, canvas[8]);
-  } else if (canvas[3] === canvas[6] && canvas[6] === canvas[9]) {
-    setStyle(3, canvas[3]);
-    setStyle(6, canvas[6]);
-    setStyle(9, canvas[9]);
-  } else if (canvas[1] === canvas[5] && canvas[5] === canvas[9]) {
-    setStyle(1, canvas[1]);
-    setStyle(5, canvas[5]);
-    setStyle(9, canvas[9]);
-  } else if (canvas[3] === canvas[5] && canvas[5] === canvas[7]) {
-    setStyle(3, canvas[3]);
-    setStyle(5, canvas[5]);
-    setStyle(7, canvas[7]);
+  // 列出贏的條件
+  let winCondictions = [
+    // 橫
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    // 直
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    // 斜
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  let index = 0;
+  while (index < winCondictions.length) {
+    let win = winCondictions[index];
+    if (
+      canvas[win[0]] === canvas[win[1]] &&
+      canvas[win[1]] === canvas[win[2]]
+    ) {
+      setStyle(win[0], canvas[win[0]]);
+      setStyle(win[1], canvas[win[1]]);
+      setStyle(win[2], canvas[win[2]]);
+      break;
+    }
+    index++;
   }
 }
+// winCondictions.forEach((win) => {
+//   if (
+//     canvas[win[0]] === canvas[win[1]] &&
+//     canvas[win[1]] === canvas[win[2]]
+//   ) {
+//     setStyle(win[0], canvas[win[0]]);
+//     setStyle(win[1], canvas[win[1]]);
+//     setStyle(win[2], canvas[win[2]]);
+//   }
+// });
+
+//   if (canvas[1] === canvas[2] && canvas[2] === canvas[3]) {
+//     setStyle(1, canvas[1]);
+//     setStyle(2, canvas[2]);
+//     setStyle(3, canvas[3]);
+//   } else if (canvas[4] === canvas[5] && canvas[5] === canvas[6]) {
+//     setStyle(4, canvas[4]);
+//     setStyle(5, canvas[5]);
+//     setStyle(6, canvas[6]);
+//   } else if (canvas[7] === canvas[8] && canvas[8] === canvas[9]) {
+//     setStyle(7, canvas[7]);
+//     setStyle(8, canvas[8]);
+//     setStyle(9, canvas[9]);
+//   } else if (canvas[1] === canvas[4] && canvas[4] === canvas[7]) {
+//     setStyle(1, canvas[1]);
+//     setStyle(4, canvas[4]);
+//     setStyle(7, canvas[7]);
+//   } else if (canvas[2] === canvas[5] && canvas[5] === canvas[8]) {
+//     setStyle(2, canvas[2]);
+//     setStyle(5, canvas[5]);
+//     setStyle(8, canvas[8]);
+//   } else if (canvas[3] === canvas[6] && canvas[6] === canvas[9]) {
+//     setStyle(3, canvas[3]);
+//     setStyle(6, canvas[6]);
+//     setStyle(9, canvas[9]);
+//   } else if (canvas[1] === canvas[5] && canvas[5] === canvas[9]) {
+//     setStyle(1, canvas[1]);
+//     setStyle(5, canvas[5]);
+//     setStyle(9, canvas[9]);
+//   } else if (canvas[3] === canvas[5] && canvas[5] === canvas[7]) {
+//     setStyle(3, canvas[3]);
+//     setStyle(5, canvas[5]);
+//     setStyle(7, canvas[7]);
+//   }
 
 // 設定顏色
 function setStyle(itemId, type) {
   let color;
-  if (type === "X") color = "green";
-  if (type === "O") color = "red";
+  if (type === "X") color = "#00916E";
+  if (type === "O") color = "#ED7777";
   document.getElementById(itemId).style.backgroundColor = color;
-  document.getElementById(itemId).style.color = "white";
+  document.getElementById(itemId).style.color = "#fffff";
   if (type) gameOver = true;
   if (gameOver)
     document.getElementById("container").style.pointerEvents = "none";
